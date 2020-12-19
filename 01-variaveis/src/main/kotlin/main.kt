b6afd2341e9614a5d489673a0be722c501b72f20
manuelernesto <manuelernesto@outlook.com>
2020-12-19 16:17:34 +0100

    definindo variaveis e tipo de dados em kotlin

---
+++ 01-variaveis/src/main/kotlin/main.kt
@@ -0,0 +1,30 @@
+fun main() {
+    val x: Int = 2
+    val z = 3
+
+    var y = 4
+    y = 5
+
+    println(x)
+    println(y)
+
+    var num1:Byte = 0
+    var num2:Short = 1
+    var num3:Int = 2
+    var num4:Long = 3
+
+    var kz = 4.0
+    var usd: Float = 5.0f
+
+    var nome: String = "Manuel"
+    var sexo: Char = 'M'
+
+    var eMairDeIdade = false
+
+    //comentario
+
+    /*
+    comentario1
+    comentario 2
+     */
+}
