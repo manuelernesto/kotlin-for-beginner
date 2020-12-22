08b383655f42195014c8057df949bfc5f9b563b9
manuelernesto <manuelernesto@outlook.com>
2020-12-22 23:44:06 +0100

    add Comparação ou relação & Aritméticos data

--- 02-operadores/src/main/kotlin/main.kt
+++ 02-operadores/src/main/kotlin/main.kt
@@ -1,3 +1,38 @@
 fun main() {
-    
+    //Aritmeticos
+    val num1 = 8
+    val num2 = 6
+
+    val sum = num1 + num2
+    println(sum)
+
+    val sub = num1 - num2
+    println(sub)
+
+    val div = num1 / num2.toDouble()
+    println(div)
+
+    val mult = num1 * num2
+    println(mult)
+
+    //Comparação ou relação
+    val comp1 = num1 > num2
+    println(comp1)
+
+    val comp2 = num1 < num2
+    println(comp2)
+
+    val comp3 =  num1 == num2
+    println(comp3)
+
+    val comp4 = num1 != num2
+    println(comp4)
+
+    val comp5 = num1 >= num2
+    println(comp5)
+
+    val comp6 = num1 <= num2
+    println(comp6)
+
+
 }
