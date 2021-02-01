889df3934eac7fb7bfff1ab7d98000e97a577f7b
manuelernesto <manuelernesto@outlook.com>
2021-02-01 02:09:18 +0100

    fluxos de repeticao

--- 04-fluxos-de-repeticao/src/main/kotlin/main.kt
+++ 04-fluxos-de-repeticao/src/main/kotlin/main.kt
@@ -1,3 +1,30 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    //for
+    for (i in 1..10) {
+        println("Kotlin")
+    }
+
+    var soma = 0
+    for (i in 1..10) {
+        soma += i // soma = soma + i
+        println(i)
+        println(soma)
+    }
+
+    print("Soma: ")
+    println(soma)
+
+    //while
+    var i = 1
+    while (i <= 10) {
+        println("Kotlin")
+        i += 1
+    }
+
+    //do while
+    var y = 11
+    do {
+        println("Kotlin 1")
+        y += 1
+    } while (y <= 10)
 }
