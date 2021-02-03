5d49d08dba7924cdbfe446447ad4c2952fac9e8c
manuelernesto <manuelernesto@outlook.com>
2021-02-03 17:28:05 +0100

    ranges

--- 05-ranges/src/main/kotlin/main.kt
+++ 05-ranges/src/main/kotlin/main.kt
@@ -1,3 +1,31 @@
 fun main() {
-    println("Hello World!")
+    //if
+    //nota >=7 && nota<=12
+    val nota = 12
+    if (nota in 7..11){
+       // println("Recurso")
+    }else{
+       // println("Outro")
+    }
+
+    //for
+    //1 ate 100
+    for (i in 1..100){
+        //println(i)
+    }
+    println("******")
+    //100 ate 1
+    for (i in 100 downTo 1){
+        //println(i)
+    }
+
+    // 1 ate 100 pulando 2 casas
+    for (i in 1..100 step 2){
+       //println(i)
+    }
+
+    // 100 ate 1 pulando 2 casas
+    for (i in 100 downTo 1 step 2){
+        println(i)
+    }
 }
