ff1208cc1d5977b2455c177a5f26c20acaeebe5c
manuelernesto <manuelernesto@outlook.com>
2021-02-22 23:44:44 +0100

    funcoes

--- 07-funcoes/src/main/kotlin/main.kt
+++ 07-funcoes/src/main/kotlin/main.kt
@@ -1,3 +1,37 @@
 fun main() {
-    
+    ola()
+    olaNome("Manuel")
+    olaNome("Ernesto")
+    val z = soma(2, 4)
+    println("Soma =  $z")
+    val y = subtracao(3, 2)
+    println("Sub $y")
+
+    val k = mult(x= 2, y = 2)
+    println("Mult = $k")
 }
+
+// funcao simples
+fun ola() {
+    println("Ola Funcao")
+}
+
+// funcao com parametro
+fun olaNome(nome: String) {
+    println("Ola $nome")
+}
+
+// funcao com retorno
+fun soma(x: Int, y: Int): Int {
+    return x + y
+}
+
+// funcao como expressao
+fun subtracao(x: Int, y: Int) = x - y
+
+// funcao com argumento padrao
+fun mult(x: Int = 10, y: Int) = x * y
+
+
+
+
